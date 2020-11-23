dd94fe367aa2f14b723c13b5cec938489cd70d6d
sabdelmagid <52176844+sabdelmagid@users.noreply.github.com>
2020-11-23 00:12:43 -0800

    Update dataloadersetup.py

--- dataloadersetup.py
+++ dataloadersetup.py
@@ -5,52 +5,15 @@ import pickle
 
 def create_dataloader(dataset_name, gt_type, img_size, 
                       batch_size=8, recompute_dataset=False):
-    if dataset_name == "cycif":
-        return create_cycif(gt_type, img_size, batch_size, recompute_dataset)
-    elif dataset_name == "cytometry":
+    
+    if dataset_name == "cytometry":
         return create_cytometry(gt_type, img_size, batch_size, recompute_dataset)
     elif dataset_name == 'synthetic':
         return create_synthetic(batch_size)
     else:
         print('wrong dataset_name')        
 
-def create_cycif(gt_type, img_size, batch_size, recompute_dataset):
-    from cycifdataloader import BioformatTiffreader, DataPreparation
-    num_classes = 3
-    prepared_dataPath = 'prepared_data_files/prepared_data_{}.pkl'.format(gt_type)
-    channels_toExclude = ['A488', 'A555', 'A555_7', 'A647', 
-                          'Hoechst2', 'Hoechst4', 'Hoechst5', 'Hoechst6',
-                          'Hoechst7', 'Hoechst8', 'Hoechst9', 'Hoechst10']
-    if recompute_dataset | (os.path.exists(prepared_dataPath) == False):
-        # define a class for data preparation
-        prepared_data = DataPreparation()
-        # resplit the dataset into train/test
-        prepared_data.split_dataset()
-        # recompute list of channels
-        prepared_data.get_channelList(channels_toExclude)
-        # recompute percentile
-        prepared_data.find_percentile()
-        # Save the data preparation obj
-        with open(prepared_dataPath, 'wb') as pkl_file:
-            pickle.dump(prepared_data, pkl_file)
-    else:
-        # load the data preparation obj
-        with open(prepared_dataPath, 'rb') as pkl_file:
-            prepared_data = pickle.load(pkl_file)
-    dataset_train = BioformatTiffreader(prepared_data, img_size=img_size,
-                                        phase_str='train')
-    dataset_test = BioformatTiffreader(prepared_data, img_size=img_size,
-                                       phase_str='test')
-    dataset_loader = dict()
-    dataset_loader['train'] = torch.utils.data.DataLoader(
-        dataset=dataset_train, batch_size=batch_size, shuffle=True,
-        drop_last=True, num_workers=8)
-    dataset_loader['test'] = torch.utils.data.DataLoader(
-        dataset=dataset_test, batch_size=batch_size, shuffle=False,
-        drop_last=False, num_workers=8)
-    num_channels = len(dataset_train.valid_channelList)
-    phase_list = ['train', 'test']
-    return dataset_loader, num_channels, num_classes, phase_list
+
 
 def create_cytometry(gt_type, img_size, batch_size, recompute_dataset):
     from cytometrydataloader import BioformatTiffreader, DataPreparation
